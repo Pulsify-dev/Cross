f1302cc7d9abad94868143153382dc85c11e7d4e
Mohammad Emad <mohammademad3012@gmail.com>
2026-03-18 22:14:44 +0200

    integrated feed provider and link with login screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:cross/core/theme/app_theme.dart';
+import 'package:cross/providers/feed_provider.dart';
 import 'package:cross/providers/profile_provider.dart';
 import 'package:cross/routes/app_routes.dart';
 import 'package:cross/routes/route_names.dart';
+import 'package:cross/features/feed/services/track_service.dart';
+import 'package:cross/features/feed/services/mock_track_service.dart';
 
 void main() {
   runApp(const PulsifyApp());
@@ -17,7 +20,17 @@ class PulsifyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ProfileProvider()),
+        Provider<TrackService>(
+          create: (_) => MockTrackService(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => FeedProvider(
+            context.read<TrackService>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ProfileProvider(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
