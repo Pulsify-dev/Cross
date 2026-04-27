e66f0096942a5294cd261e52ecd98f89e6f9c479
AliElsanan <ali.hassan05@eng-st.cu.edu.eg>
2026-04-27 16:33:16 +0300

    solve no rout define issues  in home-seaech-feed-lib screens

--- lib/features/home/screens/main_screen.dart
+++ lib/features/home/screens/main_screen.dart
@@ -160,9 +160,20 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(index: _selectedIndex, children: _screens),
-      bottomNavigationBar: _buildBottomNavigationBar(),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) return;
+        if (_selectedIndex != 0) {
+          setState(() {
+            _selectedIndex = 0;
+          });
+        }
+      },
+      child: Scaffold(
+        body: IndexedStack(index: _selectedIndex, children: _screens),
+        bottomNavigationBar: _buildBottomNavigationBar(),
+      ),
     );
   }
 }
